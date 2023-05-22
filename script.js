166782c76957cbbe9aae7a5cd657e6ea4e1c810e
jeremyimbisi <jeremyimbisi44@gmail.com>
2023-05-22 11:52:28 +0300

    info added

--- script.js
+++ script.js
@@ -1,115 +1,172 @@
-// Get the card elements
-const cards = document.querySelectorAll('.card');
-
-// Variable to track the open cards
-let openCards = [];
-
-// Variable to track the number of moves
-let moves = 0;
-
-// Variable to track the number of matched pairs
-let matchedPairs = 0;
-
-// Variable to track the timer
-let timerInterval;
-let time = 0;
-
-// Variable to track the star rating
-let starRating = 3;
-
-// Get the elements for moves, star rating, and timer
-const movesElement = document.getElementById('move-step');
-const starRatingElement = document.getElementById('Starrating');
-const timerElement = document.getElementById('timer');
-
-// Function to handle card click
-function handleCardClick() {
-  // Check if the card is already matched or open
-  if (this.classList.contains('matched') || this.classList.contains('open')) {
-    return;
+const moves = document.getElementById("moves-count");
+const timeValue = document.getElementById("time");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const gameContainer = document.querySelector(".game-container");
+const result = document.getElementById("result");
+const controls = document.querySelector(".controls-container");
+let cards;
+let interval;
+let firstCard = false;
+let secondCard = false;
+//Items array
+const items = [
+  { name: "bee", image: "fruit 3.jpg" },
+  { name: "crocodile", image: "fruit 4.jpg" },
+  { name: "macaw", image: "./" },
+  { name: "gorilla", image: "gorilla.png" },
+  { name: "tiger", image: "tiger.png" },
+  { name: "monkey", image: "monkey.png" },
+  { name: "chameleon", image: "chameleon.png" },
+  { name: "piranha", image: "piranha.png" },
+  { name: "anaconda", image: "anaconda.png" },
+  { name: "sloth", image: "sloth.png" },
+  { name: "cockatoo", image: "cockatoo.png" },
+  { name: "toucan", image: "toucan.png" },
+];
+//Initial Time
+let seconds = 0,
+  minutes = 0;
+//Initial moves and win count
+let movesCount = 0,
+  winCount = 0;
+//For timer
+const timeGenerator = () => {
+  seconds += 1;
+  //minutes logic
+  if (seconds >= 60) {
+    minutes += 1;
+    seconds = 0;
   }
-
-  // Flip the card
-  this.classList.add('open');
-
-  // Add the card to the list of open cards
-  openCards.push(this);
-
-  // Check if two cards are open
-  if (openCards.length === 2) {
-    // Disable clicking on other cards
-    cards.forEach(card => card.removeEventListener('click', handleCardClick));
-
-    // Increment the number of moves
-    moves++;
-    movesElement.textContent = moves;
-
-    // Check if the open cards match
-    if (openCards[0].innerHTML === openCards[1].innerHTML) {
-      // Cards match
-      openCards.forEach(card => card.classList.add('matched'));
-      matchedPairs++;
-
-      // Check if all pairs are matched
-      if (matchedPairs === 6) {
-        // Stop the timer
-        clearInterval(timerInterval);
-
-        // Display game completion message
-        setTimeout(() => {
-          alert(`Congratulations! You won the game in ${moves} moves and ${time} seconds.`);
-        }, 500);
+  //format time before displaying
+  let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+  let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+  timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+};
+//For calculating moves
+const movesCounter = () => {
+  movesCount += 1;
+  moves.innerHTML = `<span>Moves:</span>${movesCount}`;
+};
+//Pick random objects from the items array
+const generateRandom = (size = 4) => {
+  //temporary array
+  let tempArray = [...items];
+  //initializes cardValues array
+  let cardValues = [];
+  //size should be double (4*4 matrix)/2 since pairs of objects would exist
+  size = (size * size) / 2;
+  //Random object selection
+  for (let i = 0; i < size; i++) {
+    const randomIndex = Math.floor(Math.random() * tempArray.length);
+    cardValues.push(tempArray[randomIndex]);
+    //once selected remove the object from temp array
+    tempArray.splice(randomIndex, 1);
+  }
+  return cardValues;
+};
+const matrixGenerator = (cardValues, size = 4) => {
+  gameContainer.innerHTML = "";
+  cardValues = [...cardValues, ...cardValues];
+  //simple shuffle
+  cardValues.sort(() => Math.random() - 0.5);
+  for (let i = 0; i < size * size; i++) {
+    /*
+        Create Cards
+        before => front side (contains question mark)
+        after => back side (contains actual image);
+        data-card-values is a custom attribute which stores the names of the cards to match later
+      */
+    gameContainer.innerHTML += `
+     <div class="card-container" data-card-value="${cardValues[i].name}">
+        <div class="card-before">?</div>
+        <div class="card-after">
+        <img src="${cardValues[i].image}" class="image"/></div>
+     </div>
+     `;
+  }
+  //Grid
+  gameContainer.style.gridTemplateColumns = `repeat(${size},auto)`;
+  //Cards
+  cards = document.querySelectorAll(".card-container");
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      //If selected card is not matched yet then only run (i.e already matched card when clicked would be ignored)
+      if (!card.classList.contains("matched")) {
+        //flip the cliked card
+        card.classList.add("flipped");
+        //if it is the firstcard (!firstCard since firstCard is initially false)
+        if (!firstCard) {
+          //so current card will become firstCard
+          firstCard = card;
+          //current cards value becomes firstCardValue
+          firstCardValue = card.getAttribute("data-card-value");
+        } else {
+          //increment moves since user selected second card
+          movesCounter();
+          //secondCard and value
+          secondCard = card;
+          let secondCardValue = card.getAttribute("data-card-value");
+          if (firstCardValue == secondCardValue) {
+            //if both cards match add matched class so these cards would beignored next time
+            firstCard.classList.add("matched");
+            secondCard.classList.add("matched");
+            //set firstCard to false since next card would be first now
+            firstCard = false;
+            //winCount increment as user found a correct match
+            winCount += 1;
+            //check if winCount ==half of cardValues
+            if (winCount == Math.floor(cardValues.length / 2)) {
+              result.innerHTML = `<h2>You Won</h2>
+            <h4>Moves: ${movesCount}</h4>`;
+              stopGame();
+            }
+          } else {
+            //if the cards dont match
+            //flip the cards back to normal
+            let [tempFirst, tempSecond] = [firstCard, secondCard];
+            firstCard = false;
+            secondCard = false;
+            let delay = setTimeout(() => {
+              tempFirst.classList.remove("flipped");
+              tempSecond.classList.remove("flipped");
+            }, 900);
+          }
+        }
       }
-    } else {
-      // Cards do not match
-      setTimeout(() => {
-        openCards.forEach(card => card.classList.remove('open'));
-        openCards = [];
-      }, 1000);
-    }
-
-    // Enable clicking on other cards
-    setTimeout(() => {
-      cards.forEach(card => card.addEventListener('click', handleCardClick));
-    }, 1000);
-  }
-}
-
-// Function to start the game
-function startGame() {
-  // Shuffle the cards
-  const shuffledCards = Array.from(cards).sort(() => Math.random() - 0.5);
-
-  // Reset the game state
-  openCards = [];
-  moves = 0;
-  matchedPairs = 0;
-  starRating = 3;
-
-  // Reset the moves element
-  movesElement.textContent = moves;
-
-  // Reset the star rating element
-  starRatingElement.textContent = "*";
-
-  // Reset the timer
-  clearInterval(timerInterval);
-  time = 0;
-  timerElement.textContent = time;
-
-  // Add event listeners to the cards
-  cards.forEach(card => {
-    card.addEventListener("click", handleCardClick);
-    card.classList.remove("open", "matched");
+    });
   });
-
-  // Start the timer
-  timerInterval = setInterval(() => {
-    time++;
-    timerElement.textContent = time;
-  }, 1000);
-}
-
-// Event listener for the start button
-const startButton = document.getElementById("start-button");
-startButton.addEventListener("click", startGame);
+};
+//Start game
+startButton.addEventListener("click", () => {
+  movesCount = 0;
+  seconds = 0;
+  minutes = 0;
+  //controls amd buttons visibility
+  controls.classList.add("hide");
+  stopButton.classList.remove("hide");
+  startButton.classList.add("hide");
+  //Start timer
+  interval = setInterval(timeGenerator, 1000);
+  //initial moves
+  moves.innerHTML = `<span>Moves:</span> ${movesCount}`;
+  initializer();
+});
+//Stop game
+stopButton.addEventListener(
+  "click",
+  (stopGame = () => {
+    controls.classList.remove("hide");
+    stopButton.classList.add("hide");
+    startButton.classList.remove("hide");
+    clearInterval(interval);
+  })
+);
+//Initialize values and func calls
+const initializer = () => {
+  result.innerText = "";
+  winCount = 0;
+  let cardValues = generateRandom();
+  console.log(cardValues);
+  matrixGenerator(cardValues);
+};
